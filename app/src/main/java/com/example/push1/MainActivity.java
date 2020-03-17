c65db3196a31d6116a1d52fa6d147b430d2748a9
haotran12380 <62280123+haotran12380@users.noreply.github.com>
2020-03-17 20:16:33 +0700

    hello hao

--- app/src/main/java/com/example/push1/MainActivity.java
+++ app/src/main/java/com/example/push1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.push1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast("Hello hao");
     }
     public void test1(){
         int i = 1+ 1;
+    }
+    public void Toast(String s){
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 }
